965c1a55bd3b65b42edb667cd48d806fd817d287
hzwy23 <hzwy23@163.com>
2017-11-18 20:17:20 +0800

    fixed bug

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	// "github.com/hzwy23/apps/mas/ca"
-	// "github.com/hzwy23/apps/mas/common"
-	// "github.com/hzwy23/apps/mas/ftp"
-	"github.com/hzwy23/auth-core"
+	"github.com/hzwy23/auth"
 )
 
 func main() {
-	auth_core.AppRegister("auth", auth_core.Register)
-	// auth_core.AppRegister("ca", ca.Register)
-	// auth_core.AppRegister("common", common.Register)
-	// auth_core.AppRegister("ftp", ftp.Register)
-	auth_core.Bootstrap()
+	auth.Bootstrap()
 }
